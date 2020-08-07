9c30e6944d39fd2a49c45239a9fc631d30cf9894
mycontaktperson@gmail.com <12Q34e56t12>
2020-08-07 11:45:55 +0300

    Initialize

---
+++ AplanaCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AplanaCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
